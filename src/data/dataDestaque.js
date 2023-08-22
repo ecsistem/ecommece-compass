7f0e5e4adff39d71853673c6b68365476e9f929b
Edson Costa <edson.13.10.2004@gmail.com>
2023-08-22 09:53:02 -0300

    add data do Destaque

---
+++ src/data/dataDestaque.js
@@ -0,0 +1,20 @@
+import CardIcon from '../assets/images/Icons/card.svg';
+import LoadingIcon from '../assets/images/Icons/loading.svg';
+import CarIcon from '../assets/images/Icons/car.svg';
+export const dadosDestaque = [
+    {
+        title: 'Comprou',
+        description: 'Aceitamos todos os cartões',
+        iconImage: CardIcon
+    },
+    {
+        title: 'Atualizou',
+        description: 'Aprovação de compra',
+        iconImage: LoadingIcon
+    },
+    {
+        title: 'Entrega',
+        description: 'Entregamos para todo o Brasil',
+        iconImage: CarIcon
+    }
+];
