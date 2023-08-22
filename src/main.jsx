eb39f4dfd9679a23920045e7d18efebbe95398b3
Eduardo Kuritza <eduardo.kuritza@gmail.com>
2023-08-22 10:02:09 -0300

    CSS Global adicionado ao projeto

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './pages/Home'
+
 import './assets/css/global.css'
-import './assets/css/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
