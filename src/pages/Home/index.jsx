24b70a3f59018f59a9305b44acb42d90f04ea068
Edson Costa <edson.13.10.2004@gmail.com>
2023-08-23 11:39:31 -0300

    Header add + css

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,14 @@
 import ProductsContainer from "../../components/ProductsContainer/ProductsContainer";
 import { Destaque } from '../../components/Destaque'
 import { Desconto } from '../../components/Desconto'
+import { Header } from '../../components/Header'
 import './index.css'
 
 export function Home() {
 
   return (
     <>
+      <Header />
       <Destaque />
       <Desconto />
       <ProductsContainer />
