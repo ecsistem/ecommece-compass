e0636bf79f55de6e86f445c092801dac6ca2a1dd
cristopherkovalski <cristopherkovalski@gmail.com>
2023-08-23 13:06:14 -0300

    feat footer

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,5 @@
 import { Destaque } from '../../components/Destaque'
+import { Footer } from '../../components/Footer'
 import './index.css'
 
 export function Home() {
@@ -6,6 +7,7 @@ export function Home() {
   return (
     <>
       <Destaque />
+      <Footer />
     </>
   )
 }
