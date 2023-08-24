71c3571781117518710348832b10d6ba27a02d3f
cristopherkovalski <cristopherkovalski@gmail.com>
2023-08-24 11:59:13 -0300

    refactor banner button and width

--- src/components/LowerBanner/index.jsx
+++ src/components/LowerBanner/index.jsx
@@ -7,15 +7,22 @@ import arrow from '../../assets/images/Icons/iconArrow.svg'
 
 export function LowerBanner() {
         const [shouldRenderItemImg, setShouldRenderItemImg] = useState(true);
+        const [shouldRenderButton, setShouldRenderButton] = useState(true);
         const [bannerImage, setBannerImage] = useState(banner);
     
         useEffect(() => {
             const handleResize = () => {
                 const windowWidth = document.documentElement.clientWidth;
-                if (windowWidth <= 1650) {                    
+                if (windowWidth <= 1630) {                    
                     setShouldRenderItemImg(false);
                 } else {
                     setShouldRenderItemImg(true);
+                }
+
+                if (windowWidth <= 1200) {
+                    setShouldRenderButton(false);
+                } else {
+                    setShouldRenderButton(true);
                 }
 
                 if (windowWidth <= 800) {
@@ -37,14 +44,14 @@ export function LowerBanner() {
     return (
         <div>
             <div className="lower-banner-container">
-                <img className="lower-banner-container" src={bannerImage} alt="banner" />
+                <img className="lower-banner-img" src={bannerImage} alt="banner" />
                 {shouldRenderItemImg && <img className="lower-banner-img-card" src={itemImg} alt="item" />}
                 <div className="lower-banner-text-card ">
                     <div className="lower-banner-text-card-title">Home Office</div>
                     <div className="lower-banner-text-card-description">
                         A loja de cestos oferece-lhe todos os artigos de eletrónica ou artigos de decoração de que necessita, 
                         para além dos melhores descontos em produtos. Compre agora e aproveite todos os descontos nos produtos.</div>
-                        {shouldRenderItemImg && <button className="lower-banner-button"><img src={arrow}/> Confira</button>}
+                        {shouldRenderButton && <button className="lower-banner-button"><img src={arrow}/> Confira</button>}
                 </div>
             </div>
         </div>
